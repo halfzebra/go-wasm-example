d15c3f4336c23a80c6411bbd7ed9082d6ef8610d
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2019-04-14 19:23:44 +0200

    Include DOM manipulation

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 This is a minimal example of running Go in the browser.
 
 ```sh
+# 
 cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
+GOOS=js GOARCH=wasm go build -o main.wasm main.go
 ```
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
 	fmt.Println("Hello world")
+
+	document := js.Global().Get("document")
+	p := document.Call("createElement", "p")
+	p.Set("innerHTML", "Hello WASM from Go!")
+	document.Get("body").Call("appendChild", p)
 }
